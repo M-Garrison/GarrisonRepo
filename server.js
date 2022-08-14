fe5b462001afddc584ae02f95d4d54f62e1e77d1
M-Garrison <103372479+M-Garrison@users.noreply.github.com>
2022-08-13 20:46:53 -0500

    edit and update routes added

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const Meme = require('./models/memes.js');
 const app = express ();
 const db = mongoose.connection;
 require('dotenv').config()
+
 //___________________
 //Port
 //___________________
@@ -21,11 +22,13 @@ const PORT = process.env.PORT || 3003;
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI;
 
+//___________________
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
 mongoose.connect(MONGODB_URI);
 
+//___________________
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
@@ -40,18 +43,32 @@ app.use(express.static('public'));
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
 app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 
 //___________________
-// Routes
+// 7 ROUTES:
+//___________________
 
+//___________________
+// EDIT ROUTE
+app.get('/fantasy/:id/edit', (req, res) => {
+  Meme.findById(req.params.id, (err, foundMeme) => {
+      res.render('edit.ejs', {
+          bongo: foundMeme
+      });        
+  });
+});
+
+//___________________
+// NEW ROUTE
 app.get('/fantasy/new', (req, res) => {
   res.render('new.ejs')
 });
 
+//___________________
+// SHOW ROUTE
 app.get('/fantasy/:id', (req, res) => {
   Meme.findById(req.params.id, (error, foundMeme) => {
     res.render(
@@ -63,6 +80,22 @@ app.get('/fantasy/:id', (req, res) => {
   })
 }); 
 
+//___________________
+// PUT ROUTE
+app.put('/fantasy/:id', (req, res)=>{
+  if(req.body.motherKnow === 'on'){
+      req.body.motherKnow = true;
+  } else {
+      req.body.motherKnow = false;
+  }
+  Meme.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedModel) => {
+    res.redirect('/fantasy')
+  })
+});
+
+
+//___________________
+// CREATE ROUTE
 app.post('/fantasy', (req, res) => {
   if(req.body.motherKnow === "on"){
     req.body.motherKnow = true;
@@ -75,6 +108,8 @@ app.post('/fantasy', (req, res) => {
   // res.send(req.body); 
 });
 
+//___________________
+// INDEX ROUTE
 app.get('/fantasy', (req, res) => {
   Meme.find({}, (error, allMemes) => {
     res.render(
@@ -85,8 +120,6 @@ app.get('/fantasy', (req, res) => {
     )
   })
 });
-
-
 
 
 //___________________
